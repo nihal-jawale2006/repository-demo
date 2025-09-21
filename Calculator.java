4f1350e8866e0ad753156ee5cf1eaeca7c81e4f2
Nihal Jawale <jawalenihal2006@gmail.com>
2025-09-21 16:05:19 +0530

    Simple Calculator using java.

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Calci {
+public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         char choice;
